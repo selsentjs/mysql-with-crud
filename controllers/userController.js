37528bb833ffc40329d79c8c9f4391f6dfabd26f
Selvi <reachyourgoal2006@gmail.com>
2023-01-17 18:10:49 +0530

    updataUser,deleteUser are completed

--- controllers/userController.js
+++ controllers/userController.js
@@ -34,40 +34,58 @@ const getSingleUser = async (req, res) => {
 // create new user
 
 const createUser = async (req, res) => {
-try{
-    const {name, email} = req.body;
-    if(name.trim()!="" && email.trim()!=""){
-        let sql = await "INSERT INTO USER(name, email) VALUES('"+
+  try {
+    const { name, email } = req.body;
+    if (!name && !email) {
+      let sql =
+        (await "INSERT INTO USER(name, email) VALUES('") +
         name +
         "', '" +
         email +
-         "')";
-        con.query(sql, (err,user) => {
-            if(err){
-                throw err
-            }
-            res.status(200).json({msg:'user created', user})
-        })
+        "')";
+      con.query(sql, (err, user) => {
+        if (err) {
+          throw err;
+        }
+        res.status(200).json({ msg: "user created", user });
+      });
+    } else {
+      res.send("All the fields are required");
     }
-    else{
-        res.send('All the fields are required')
-    }
-
-}
-catch(err) {
-    res.status(500).json({msg:err})
-}
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
 };
 
 // update user
 const updateUser = async (req, res) => {
-  res.send("update user");
+  try {
+    let id = req.params.id;
+    let { name, email } = req.body;
+    let sql = await "UPDATE USER SET name=?, email=? where id=?";
+    console.log("sql", sql);
+    con.query(sql, [name, email, id], (err) => {
+      if (err) throw err;
+      res.status(200).json({ msg: "data has been updated" });
+    });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
 };
 
 // delete user
 
 const deleteUser = async (req, res) => {
-  res.send("delete user");
+  try {
+    let id = req.params.id;
+    let sql = await "DELETE FROM USER where id=?";
+    con.query(sql, id, (err) => {
+      if (err) throw err;
+      res.status(200).json({ msg: "data deleted successfully" });
+    });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
 };
 
 module.exports = {
